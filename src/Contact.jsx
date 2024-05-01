162c9b2bf6b5a64873c4b4f6724861c65a0083f5
Rotimi Babalola <feyibabalola2000@gmail.com>
2024-04-30 17:15:17 -0700

    add

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,6 +8,7 @@ const Contact = () => {
       <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[50%]" placeholder="Enter Your Full Name" />
       <input type="email" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[50%]" placeholder="Enter Your Email Address" />
       <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[50%]" placeholder="Enter Your Desired Design" />
+      <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[50%]" placeholder="Enter Your Location" />
 
       
     </div>
