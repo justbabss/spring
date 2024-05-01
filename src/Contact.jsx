45e64e286ffdb0efe3b90b8a1fc4df9fa4b9d09b
Rotimi Babalola <feyibabalola2000@gmail.com>
2024-04-30 22:37:43 -0700

    add

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,16 +1,50 @@
+import  { useState } from 'react';
+import Select from 'react-select';
 
 
 const Contact = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const countryCodes = [
+    { label: 'Afghanistan (+93)', value: '+93' },
+    { label: 'Albania (+355)', value: '+355' },
+    { label: 'Algeria (+213)', value: '+213' },
+    // Add more countries...
+  ];
+  
+    const handleChange = (selectedOption) => {
+      setSelectedCountry(selectedOption);
+    };
+
   return (
-    <div className="  h-screen">
+    <div className=" mx-[1.5rem] h-full">
       <h1 className=" mt-[2rem] font-bold text-[1.4rem] text-center">Contact Us @ Spring</h1>
-      
-      <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[50%]" placeholder="Enter Your Full Name" />
-      <input type="email" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[50%]" placeholder="Enter Your Email Address" />
-      <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[50%]" placeholder="Enter Your Desired Design" />
+
+      <p className=" md:w-[60%] mx-auto mt-5 text-center">Thank you for considering to contact us. Before reaching out, we recommend checking our FAQ section to see if your question has already been answered. You may find the information you need without having to wait for a response.
+        If the answer to you question is not in the FAQ and still need to get in touch, please feel free to fill out the contact form below. We strive to respond to all inquiries promptly and look forward to assisting you.</p>
+      <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[100%] md:w-[50%]" placeholder="Enter Your Full Name" />
+      <input type="email" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[100%] md:w-[50%]" placeholder="Enter Your Email Address" />
+      <div className="relative">
+      <Select
+        className="absolute top-full w-[100%] mx-auto md:w-[50%] border border-[#F6DA6F] bg-white z-10 outline-none"
+        options={countryCodes}
+        value={selectedCountry}
+        onChange={handleChange}
+        placeholder="Select Country Code"
+      />
+      <input
+        type="text"
+        name="phone"
+        id="phone"
+        className="block border-2 border-[#F6DA6F] mx-auto outline-none p-2 my-[2rem] w-[100%] md:w-[50%]"
+        placeholder="Enter Your Phone Number"
+        value={selectedCountry ? selectedCountry.value : ''}
+      />
+    </div>
+      <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[100%] md:w-[50%]" placeholder="Enter Your Project Name" />
+      <input type="text" name="" id="" className=" block border-2 border-[#F6DA6F] mx-auto justify-center outline-none p-2 my-[2rem] w-[100%] md:w-[50%]" placeholder="Enter Your Desired Design" />
      
 
-      
+      <button className=" flex bg-black text-white p-[0.3rem] mx-auto  px-4 mt-6">Submit</button> 
     </div>
   )
 }
--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -1,6 +1,92 @@
+import tick from './assets/tick.png'
+
 const Pricing = () => {
     return ( 
-        <section> Pricing </section>
+        <section  className=" mx-[1.5rem] h-full">
+            <h1 className=" mt-[2rem] font-bold text-[1.5rem] text-center">Pricing</h1>
+            <p className=" md:w-[60%] mx-auto mt-3 text-[1.2rem] text-center">Pricing is the process whereby a business sets the price at which it will sell its products and services, and may be part of the business marketing plan.</p>
+
+            <div className=" md:flex justify-around gap-[3rem] mt-[3rem] md:mt-[5rem]">
+                <div className=" bg-[#F0F7FD] md:w-[35%] md:h-[41.5rem]">
+                    <h1 className=" font-bold text-[1.2rem] mt-5 md:ml-8 ml-4 pt-3">Basic</h1>
+                    <p className=" text-[1.2rem] mt-3 md:ml-8 ml-4">Start with a first step</p>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>Prototype</h1>
+                            <p className=' mt-2 text-[1.0rem]'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                    <hr className=' border-[#9E9E9E] w-[90%] mx-[1.25rem] mt-4'/>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>App Design</h1>
+                            <p className=' mt-2 text-[1.0rem]'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                    <hr className=' border-[#9E9E9E] w-[90%] mx-[1.25rem] mt-4'/>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>Web Design</h1>
+                            <p className=' mt-2 text-[1.0rem]'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                    <hr className=' border-[#9E9E9E] w-[90%] mx-[1.25rem] mt-4'/>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>Interaction</h1>
+                            <p className=' mt-2 text-[1.0rem] pb-8'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className=" bg-[#F0F7FD] md:w-[35%] mt-[5rem] md:mt-0 md:h-[41.5rem]">
+                    <h1 className=" font-bold text-[1.2rem] mt-5 md:ml-8 ml-4 pt-3">Pro</h1>
+                    <p className=" text-[1.2rem] mt-3 md:ml-8 ml-4">Make things happen</p>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>Prototype</h1>
+                            <p className=' mt-2 text-[1.0rem]'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                    <hr className=' border-[#9E9E9E] w-[90%] mx-[1.25rem] mt-4'/>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>App Design</h1>
+                            <p className=' mt-2 text-[1.0rem]'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                    <hr className=' border-[#9E9E9E] w-[90%] mx-[1.25rem] mt-4'/>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>Web Design</h1>
+                            <p className=' mt-2 text-[1.0rem]'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                    <hr className=' border-[#9E9E9E] w-[90%] mx-[1.25rem] mt-4'/>
+
+                    <div className=' flex gap-[0.5rem] mt-10 ml-2 md:ml-5'>
+                        <img src={tick} alt="" className=' w-[2rem] h-[2rem]' />
+                        <div>
+                            <h1 className=' text-[#4197E0] font-semibold'>Interaction</h1>
+                            <p className=' mt-2 text-[1.0rem] pb-8'>A simple plan for simple needsd. Use one of our design to create a contract.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
      );
 }
  
